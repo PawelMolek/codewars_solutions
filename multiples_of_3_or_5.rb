e3b0da6ef237f41e4f842ddfa9336ded20896a56
PawelMolek <pawel.molek@gmail.com>
2022-02-19 10:50:21 +0100

    Multiples of 3 or 5 soltuion

---
+++ multiples_of_3_or_5.rb
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/514b92a657cdc65150000006/train/ruby
+def solution(number)
+  x=0
+  sum = 0
+  while x < number
+    if ( x % 3 == 0  or  x % 5 == 0 )
+      sum += x
+    end
+    x+=1
+  end
+  sum
+end
+
+
