1d64aca2b8a8d347c5ee4ae7b56e6cd507a17a10
PawelMolek <pawel.molek@gmail.com>
2022-02-28 11:25:17 +0100

    Who likes it solution

--- who_likes_it.rb
+++ who_likes_it.rb
@@ -1,17 +1,17 @@
 # https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/ruby
 def likes(names)
   length = names.length
-  if length == 1
-    return "#{names[0]} likes this"
-  elsif length == 2
-    return "#{names[0]} and #{names[1]} like this"
-  elsif length == 3
-    return "#{names[0]}, #{names[1]} and #{names[2]} like this"
-  elsif length == 0
+  case length
+  when 0
     return "no one likes this"
+  when 1
+    return "#{names[0]} likes this"
+  when 2
+    return "#{names[0]} and #{names[1]} like this"
+  when 3
+    return "#{names[0]}, #{names[1]} and #{names[2]} like this"
   else
     return "#{names[0]}, #{names[1]} and #{length - 2} others like this"
   end
 end
 
-puts likes(['Alex', 'Jacob', 'Mark', 'Max'])
